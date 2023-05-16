cec81f77c0ff6857208f4bead7e8c0a2527222b1
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-05-16 17:36:03 +0200

    Up_date 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import *
 
 def main():
-    a, operator, b = input().split()
+    try:
+        a, operator, b = input().split()
+        a = int(a)
+        b = int(b)
 
-    if len(a) == 0 or len(operator) == 0 or len(b) == 0:
+        if operator == "+":
+            print(f"{a} + {b} = {add(a, b)}")
+        elif operator == "-":
+            print(f"{a} - {b} = {subtract(a, b)}")
+        elif operator == "*":
+            print(f"{a} * {b} = {multiply(a, b)}")
+        elif operator == "/":
+            print(f"{a} / {b} = {divide(a, b)}")
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+    except ValueError:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    a = int(a)
-    b = int(b)
-
-    if operator == "+
-        result = calculator_1.add(a, b)
-    elif operator == "-":
-        result = calculator_1.subtract(a, b)
-    elif operator == "*":
-        result = calculator_1.multiply(a, b)
-    elif operator == "/":
-        result = calculator_1.divide(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    print(f"{a} {operator} {b} = {result}")
 
 if __name__ == "__main__":
     main()
