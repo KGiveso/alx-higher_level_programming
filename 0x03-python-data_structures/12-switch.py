6c7adc5c3a4dacd1163bc511c8c7ef5b8a63aef8
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-05-16 12:52:54 +0200

    Updated 12-switch.py

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 a = 89
+
 b = 10
-# Insert your code here (exactly 1 line)
+
 a, b = b, a
-print("a = {:d}".format(a))
-print("b = {:d}".format(b))
+
+print("a={:d} - b={:d}".format(a, b))
