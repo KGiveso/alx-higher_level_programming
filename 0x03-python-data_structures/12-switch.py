22b968f54685322af2ce550db5022ba46431c334
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-05-16 12:45:36 +0200

    Updated1 12-switch.py

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 a = 10
 b = 20
 # Insert your code here (exactly 1 line)
 a, b = b, a
-print("a =", a, end=" ")
-print("b =", b)
+print("a = {:d}".format(a))
+print("b = {:d}".format(b))
