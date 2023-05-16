6d81179191ad0e41c04c563e9876748713506f9d
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-05-16 09:30:28 +0200

    Updated1 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-  """Prints a matrix of integers.
+    """
+    Prints a matrix of integers
+    ...
 
-  Args:
-    matrix: A list of lists of integers.
+    Parameters
+    ----------
+    matrix : list (of lists)
+        The list to print
 
-  Raises:
-    ValueError: If the matrix is not rectangular.
+    Return:
+        None
+    """
 
-  """
-  if not matrix:
-    return
-
-  num_rows = len(matrix)
-  num_cols = len(matrix[0])
-
-  for row in range(num_rows):
-    for col in range(num_cols):
-      print("{0:5d}".format(matrix[row][col]), end=" ")
-    print()
-
-
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
