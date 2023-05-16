7cfccc1a51dd124f5d90422343bb98448c8632fb
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-05-16 12:36:55 +0200

    Create 12-switch.py

---
+++ 0x03-python-data_structures/12-switch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def switch_values(a, b):
+
+    # Insert your code here
+
+    a, b = b, a
+
+    return a, b
+
+# Test the function
+
+a = 5
+
+b = 10
+
+a, b = switch_values(a, b)
+
+print(a, b)  # Output: 10 5
