8fc16a6888990d6a372752a2627d2dc66294b22f
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-06-01 02:12:00 +0200

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""Square class that defines a square"""
-
+"""Write class square that defines a square"""
+"""__init__ - initialize
+    size - size of the square
+"""
 
 class Square:
-    """__init__ - initialize a square class
-
-    Args:
-    size - size of the square
-
-    """
+    
     def __init__(self, size):
         self.__size = size
