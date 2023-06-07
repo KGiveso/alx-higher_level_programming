034527455dee360a73b6914b6ee2c9cfc614bb8c
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-06-07 16:46:39 +0200

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""Write a function that prints a text with 2 new lines after each char ., ? and :
-"""
+"""Text indentation"""
 
 
-def max_integer(list=[]):
-    """Strings: list of integer
-        Return:
-            If the list is empty, the function returns None
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of
+    of these character: ., ? and :
+
+    Raise:
+        raises TypeError if text is not a string
     """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    check_space = True
+    for char in text:
+        if check_space is True and char == " ":
+            continue
+        check_space = False
+        print(char, end="")
+        if char in ["?", ".", ":"]:
+            check_space = True
+            print("\n")
