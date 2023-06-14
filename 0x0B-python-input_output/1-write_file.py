a5b402e98dad28d19ff9d46254fb88b727119613
KGIveso <mogoboyagiven@gmail.com>
2023-06-13 17:01:44 -0700

    mandatory

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""write to a file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns the number
+    of characters written"""
+
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
